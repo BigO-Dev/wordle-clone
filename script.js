430859c3fafd3ca5209996a45e14d09e068e5c9f
Omair Majid <omajid.dev@gmail.com>
2024-05-17 17:19:47 -0700

    feat: Add favicon and implement letter guessing functionality

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-function init() {
- const 
-  console.log('Hello, World!')
+const tiles = document.querySelectorAll('.board__tile')
+
+let currentGuess = ''
+
+async function getWord() {
+  const api = 'https://words.dev-apis.com/word-of-the-day'
+
+  const response = await fetch(api)
+  const data = await response.json()
+  const word = data.word
+
+  console.log('Correct word: ', word)
+  return word
 }
 
-init()
+function listenForKeys() {
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace') {
+      currentGuess = currentGuess.slice(0, -1)
+      updateUI()
+    } else if (event.key === 'Enter') {
+      checkGuess()
+    } else {
+      checkLetter(event.key)
+    }
+  })
+}
+
+function checkLetter(letter) {
+  // Checks if the key pressed is a letter
+  if (letter.match(/[a-z]/i)) {
+    currentGuess += letter
+    updateUI()
+  }
+}
+
+function updateUI() {
+  tiles.forEach((tile, index) => {
+    tile.textContent = currentGuess[index] || ''
+  })
+}
+
+async function checkGuess() {
+  let word = await getWord()
+
+  if (currentGuess === word) {
+    console.log('Correct!')
+  } else {
+    console.log('Incorrect!')
+  }
+}
+
+listenForKeys()
